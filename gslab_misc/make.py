ae9e7d8f95fbe922b3ecee1ceffb0e67716fdc97
M-R-Sullivan <m.r.sullivan@stanford.edu>
2017-06-28 16:35:57 -0700

    #4: Revised some tests

--- gslab_misc/make.py
+++ gslab_misc/make.py
@@ -1,43 +1,36 @@
-#! /usr/bin/env python
-#****************************************************
-# GET LIBRARY
-#****************************************************
-import subprocess, shutil, os
-gslab_make_path = os.getenv('gslab_make_path')
-subprocess.call('svn export --force -r 33502 ' + gslab_make_path + ' gslab_make', shell = True)
-from gslab_make.py.get_externals import *
-from gslab_make.py.make_log import *
-from gslab_make.py.run_program import *
-from gslab_make.py.dir_mod import *
+import os
+import gslab_scons as gs
 
-#****************************************************
-# MAKE.PY STARTS
-#****************************************************
 
-# DEFINE FILE/DIRECTORY LOCATIONS
-set_option(makelog = 'log/make.log', output_dir = './log', temp_dir = '', external_dir = './external')
+def main():
+    log = 'log/make.log'
+    if os.path.isfile(log):
+        os.remove(log)
 
-clear_dirs('./external/', './log/')
-start_make_logging()
+    scripts = ['insert_tag', 'center_estimates', 'select_observations',
+               'testbad',    'save_data',        'preliminaries', 
+               'testgood',   'aivreg',           'build_recode_template',
+               'plotcoeffs', 'load_and_append',]
+    for script in scripts:
+        run_test('test/test_%s.do' % script, log)
 
-get_externals('externals.txt')
 
-# RUN ALL TESTS
-run_stata(program = 'test/test_testgood.do', changedir = True)
-run_stata(program = 'test/test_testbad.do', changedir = True)
-run_stata(program = 'test/test_save_data.do', changedir = True)
-run_stata(program = 'test/test_preliminaries.do', changedir = True)
-run_stata(program = 'test/test_select_observations.do', changedir = True)
-run_stata(program = 'test/test_build_recode_template.do', changedir = True)
-run_stata(program = 'test/test_insert_tag.do', changedir = True)
-run_stata(program = 'test/test_load_and_append.do', changedir = True)
-run_stata(program = 'test/test_plotcoeffs.do', changedir = True)
-run_stata(program = 'test/test_center_estimates.do', changedir = True)
-run_stata(program = 'test/test_aivreg.do', changedir = True)
+def run_test(script, log):
+    '''Run a test and append results to log.'''
+    gs.build_stata(source = script, 
+                   target = log, 
+                   env    = {'user_flavor': None})
+    log_dir = os.path.dirname(log)
+    
+    # Read the log for the script
+    script_log = '%s/sconscript.log' % log_dir
+    log_contents = open(script_log, 'rU').read()
+    
+    # Add log for individual script to the master log
+    with open(log, 'ab') as f:
+        f.write(log_contents)
 
-shutil.rmtree('test/temp')
+    os.remove(script_log)
 
-end_make_logging()
 
-shutil.rmtree('gslab_make')
-raw_input('\n Press <Enter> to exit.')
+main()
